1fed830d3df62f8f21f4411e3b6866addbff860d
AlexNovoros87 <kyockgb@gmail.com>
2024-06-07 12:53:06 +0300

    new branch

--- sprint1/problems/final_task/solution/src/main.cpp
+++ sprint1/problems/final_task/solution/src/main.cpp
@@ -65,7 +65,6 @@ int main(int argc, const char *argv[])
 
         // Эта надпись сообщает тестам о том, что сервер запущен и готов обрабатывать запросы
         std::cout << "Server has started..."sv << std::endl;
-        // std::abort();
         //  6. Запускаем обработку асинхронных операций
 
         RunWorkers(std::max(1u, num_threads), [&ioc]

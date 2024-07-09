8ce27a0534f795b9ae5a7fcef59e393613e000f4
AlexNovoros87 <kyockgb@gmail.com>
2024-07-09 17:50:28 +0300

    fin task2

---
+++ sprint2/problems/command_line/solution/src/model/game_dog_session.h
@@ -0,0 +1,130 @@
+#pragma once
+#include "infrastructure.h"
+#include <stdexcept>
+#include <memory>
+#include <iostream>
+#include <iostream>
+#include <map>
+#include <mutex>
+
+namespace model
+{
+
+   class Dog
+    {
+
+    public:
+        Dog() = default;
+        Dog(std::string name);
+        const std::string &GetName() const;
+        int GetId() const;
+        void ChangeName(std::string name);
+        const DogCoordinates& GetDogCoord() const;
+        const DogSpeed& GetDodSpd() const;
+        void SetDogSpeedAndDirection(DogSpeed spd, NSWE_Direction dir);
+        DogCoordinates& SetDogCoordinates(double x, double y);
+        DogCoordinates& SetDogCoordinates(DogCoordinates coord);
+        void IsNeededTStopDog(const DogCoordinates& coords, const Limits &lim)
+        {  
+             if(IsLessEqual(coords.x , lim.xmin) &&   std::isless(speed_.speed_x , 0.)){StopDog();}
+             if(IsGreaterEqual(coords.x , lim.xmax) && std::isgreater(speed_.speed_x, 0.) ){StopDog();}
+             if(IsLessEqual (coords.y  , lim.ymin)  && std::isless(speed_.speed_y , 0.)){StopDog();}
+             if(IsGreaterEqual( coords.y  , lim.ymax)  &&  std::isgreater(speed_.speed_y , 0. )){StopDog();}
+                  
+        }
+        NSWE_Direction GetOrientation() const;
+        void StopDog(){
+            speed_ = {0.,0.};
+        } 
+   
+        void ManualTick(double delta_t){
+
+
+
+
+            
+        }
+    
+    
+    
+    
+    
+    
+    private: 
+        std::string name_;
+        static uint32_t ids_;
+        uint32_t id_;
+        DogCoordinates coordinates_{0.,0.};
+        DogSpeed speed_ {0.,0.};
+        NSWE_Direction direction_ = NSWE_Direction::NORTH;
+      
+    };
+    
+
+    class GameSession
+    {
+    public:
+        GameSession(Map mp);
+        std::shared_ptr<Dog> JoinGame(std::shared_ptr<Dog> dog_ptr);
+        void LeaveGame(uint32_t id);
+        const std::map<uint32_t, std::shared_ptr<Dog>> &GetDogs() const;
+        void PrintDogs() const;
+        double GetSessionSpeed() const;
+        const model::Map& GetMap() const;
+  private:
+        model::Map map_;
+        std::map<uint32_t, std::shared_ptr<Dog>> dogs_;
+        int players = 0;
+      
+    };
+
+    class Game
+    {
+    public:
+        using Maps = std::vector<Map>;
+        void AddMap(Map map);
+        const Maps &GetMaps() const noexcept;
+        const Map *FindMap(const Map::Id &id) const noexcept;
+
+    private:
+        using MapIdHasher = util::TaggedHasher<Map::Id>;
+        using MapIdToIndex = std::unordered_map<Map::Id, size_t, MapIdHasher>;
+         std::vector<Map> maps_;
+         MapIdToIndex map_id_to_index_;
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

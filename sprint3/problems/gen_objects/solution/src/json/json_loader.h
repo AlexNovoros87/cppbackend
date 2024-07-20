e726bafb07811adad2f02622432735476a52c3b3
AlexNovoros87 <kyockgb@gmail.com>
2024-07-20 12:20:57 +0300

    gen obj

---
+++ sprint3/problems/gen_objects/solution/src/json/json_loader.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <boost/json.hpp>
+#include <filesystem>
+#include "../game/game.h"
+
+
+struct Literals
+{
+    Literals() = delete;
+    static constexpr const boost::json::string_view X = "x";
+    static constexpr const boost::json::string_view Y = "y";
+    static constexpr const boost::json::string_view W = "w";
+    static constexpr const boost::json::string_view H = "h";
+    static constexpr const boost::json::string_view X0 = "x0";
+    static constexpr const boost::json::string_view Y0 = "y0";
+    static constexpr const boost::json::string_view X1 = "x1";
+    static constexpr const boost::json::string_view Y1 = "y1";
+    static constexpr const boost::json::string_view OFFSETX = "offsetX";
+    static constexpr const boost::json::string_view OFFSETY = "offsetY";
+    static constexpr const boost::json::string_view ROADS = "roads";
+    static constexpr const boost::json::string_view BUILDINGS = "buildings";
+    static constexpr const boost::json::string_view OFFICES = "offices";
+    static constexpr const boost::json::string_view MAPS = "maps";
+    static constexpr const boost::json::string_view ID = "id";
+    static constexpr const boost::json::string_view NAME = "name";
+    static constexpr const boost::json::string_view defaultDogSpeed = "defaultDogSpeed";
+    static constexpr const boost::json::string_view dogSpeed = "dogSpeed";
+    static constexpr const boost::json::string_view lootTypes = "lootTypes";
+    static constexpr const boost::json::string_view file = "file";
+    static constexpr const boost::json::string_view type = "type";
+    static constexpr const boost::json::string_view rotation= "rotation";
+    static constexpr const boost::json::string_view color= "color";
+    static constexpr const boost::json::string_view scale = "scale";
+    static constexpr const boost::json::string_view period = "period" ;
+    static constexpr const boost::json::string_view probability = "probability";
+    static constexpr const boost::json::string_view lootGeneratorConfig = "lootGeneratorConfig";
+};
+
+//#include"../model/game_dog_session.h"
+namespace json = boost::json;
+namespace json_loader {
+     api::Play LoadPlay(const std::filesystem::path& json_path, bool random, bool auto_t);
+
+}  // namespace json_loader
---
+++ sprint3/problems/gen_objects/solution/src/req_helper/help.h
@@ -0,0 +1,52 @@
+#pragma once
+//#include"request_handler.h"
+#include <sstream>
+#include <string>
+#include <filesystem>
+#include <algorithm>
+
+#include "../server/http_server.h"
+#include "../game/game.h"
+#include "req_static_objects.h"
+
+
+
+namespace request_handler{
+
+bool IsSubPath(fs::path path, fs::path base);
+bool CheckBaseValid(const std::vector<std::string> &container);
+bool IsCorrectWay(std::string_view way);
+
+std::string MakeOneMap(const model::Map *map);
+std::string MakeAllMaps(const model::Game &game);
+std::string GetExtention(std::string req_body);
+std::string CodeMessageBody(std::string code, std::string message);
+std::string TokenIdBody(std::string token, uint64_t id);
+
+std::vector<std::string> ParseToken(std::string target);
+std::vector<std::string> ParseTarget(std::string target);
+TypeRequest GetTypeRequset(const std::vector<std::string>& vec);
+DirectionAPI Director(const std::vector<std::string> &container);
+TimePoint GetNow();
+}
+
+namespace request_handler{
+
+StringResponse Make400(unsigned version, bool alive);
+StringResponse Make404(unsigned version, bool alive);
+
+}
+
+namespace request_handler{
+     //JSB - JSon-Body
+     StringResponse Template(unsigned version, bool alive);
+     StringResponse Make200JSB(unsigned version, bool alive, std::string token, uint32_t id);
+     StringResponse Make200JSB(unsigned version, bool alive, std::string body);
+     StringResponse Make200State(unsigned version, bool alive, std::shared_ptr<model::GameSession> session);
+     StringResponse Make400JSB(unsigned version, bool alive, std::string code, std::string message);
+     StringResponse Make401JSB(unsigned version, bool alive, std::string code, std::string message);
+     StringResponse Make404JSB(unsigned version, bool alive, std::string code, std::string message);
+     StringResponse Make405JSB(unsigned version, bool alive, std::string code, std::string message, std::string allow);
+     std::string MakeStateBody(std::shared_ptr<model::GameSession> session_);
+     
+}
